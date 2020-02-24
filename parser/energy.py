ce407b64fcb9cbf5e129194dcf75bde4ce6e366d
Fajar Alif <alifrdu@gmail.com>
2020-02-25 04:03:34 +0700

    add parser for fermi and band energies

---
+++ parser/energy.py
@@ -0,0 +1,29 @@
+"""energy.py
+This will extract fermi and band energies from OUTCAR file from VASP calculation.
+
+Examples of information that we need to extract:
+ BZINTS: Fermi energy: -0.055695; 50.000000 electrons
+         Band energy:-106.620542;  BLOECHL correction: -0.111340
+
+Output that we need:
+-0.055695
+-106.620542
+"""
+import sys
+import re
+
+outcar = sys.argv[1]
+output = sys.argv[2]
+energy = []
+for line in reversed(open(outcar).readlines()):
+    if line.startswith(' BZINTS:'):
+        energy.append(line.split())
+        break
+    if line.startswith('         Band energy:'):
+        energy.append(line.split())
+energy = [energy[0][1], energy[1][3]]
+with open(output, 'w') as f:
+    for e in reversed(energy):
+        e = re.sub('\ |\?|\!|\/|\;|\:|\energy', '', e)
+        f.write(''.join(e))
+        f.write('\n')
