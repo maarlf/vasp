bb1fe594b6a4b4d88c1ac643fc8c2c716874c39e
Fajar Alif <alifrdu@gmail.com>
2020-07-12 16:11:57 +0700

    modify classifier script

---
+++ matsvm/matsvm.py
@@ -0,0 +1,59 @@
+import sys
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+
+if __name__ == '__main__':
+    import warnings
+    warnings.filterwarnings('ignore')
+
+    # Input arguments
+    train_data = sys.argv[1]
+    test_data = sys.argv[2]
+
+    # Load datasets
+    print()
+    print('==== Load datasets ====')
+
+    df_train = pd.read_csv(train_data)
+    df_test = pd.read_csv(test_data)
+
+    X_train = df_train.drop(['structure', 'class'], axis=1)
+    y_train = df_train['class'].values
+
+    X_test = df_test.drop(['structure', 'class'], axis=1)
+    y_test = df_test['class'].values
+
+    print()
+    print('Done: datatrain.csv, datatest.csv')
+
+    # ML
+    print()
+    print('==== Training machine learning ====')
+    print()
+
+    kernel = ['linear', 'rbf', 'poly']
+
+    param_grid = {
+        'kernel': kernel,
+        'C': [0.1, 0.5, 1, 10, 30, 40, 50, 70, 100, 500, 1000],
+        'gamma': [0.0001, 0.001, 0.005, 0.01, 0.05, 0.07, 0.1, 0.5, 1, 5, 10, 50]
+    }
+    
+    grid = GridSearchCV(estimator=SVC(), param_grid=param_grid, cv=5, refit=True)
+    grid.fit(X_train, y_train)
+
+    print('Best score: {}'.format(grid.score(X_test, y_test)))
+    print('Best params: {}'.format(grid.best_params_))
+
+    # Result
+    print()
+    print('==== Result ====')
+    print()
+
+    y_pred = grid.predict(X_test)
+    print('SVM Accuracy with best params: {}%'.format(accuracy_score(y_pred, y_test) * 100))
+    print('Actual test label:    {}'.format(y_test))
+    print('Predicted test label: {}'.format(y_pred))
