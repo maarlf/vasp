47022b66fa3fa57a1af9c2c86f73fde4ddf28774
Fajar Alif <alifrdu@gmail.com>
2020-07-15 20:52:51 +0700

    add some data preprocessing

--- matsvm/matsvm.py
+++ matsvm/matsvm.py
@@ -1,8 +1,11 @@
 import sys
+
 import pandas as pd
-from sklearn.model_selection import GridSearchCV
+
+from sklearn.preprocessing import Normalizer
+from sklearn.model_selection import GridSearchCV, KFold
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix
 
 
 if __name__ == '__main__':
@@ -14,63 +17,85 @@ if __name__ == '__main__':
     test_data = sys.argv[2]
 
     # Load datasets
-    print()
     print('==== Load datasets ====')
+
+    normalizer = Normalizer()
 
     df_train = pd.read_csv(train_data)
     df_test = pd.read_csv(test_data)
 
-    X_train = df_train.drop(['structure', 'class'], axis=1)
+    X_train = df_train.drop(['a', 'b', 'x', 'class'], axis=1)
+    X_train_norm = normalizer.fit_transform(X_train)
     y_train = df_train['class'].values
 
-    X_test = df_test.drop(['structure', 'class'], axis=1)
+    X_test = df_test.drop(['a', 'b', 'x', 'class'], axis=1)
+    X_test_norm = normalizer.fit_transform(X_test)
     y_test = df_test['class'].values
 
-    structures = df_test['structure'].to_list()
+    selected = ['m1', 'm2', 'm3', 'mtot', 'n_el', 'tot_en', 'z3', 'z5', 'z6', 'z7']
+    X_train2 = df_train[selected]
+    X_train2_norm = normalizer.fit_transform(X_train2)
 
-    print()
+    X_test2 = df_test[selected]
+    X_test2_norm = normalizer.fit_transform(X_test2)
+
     print('Done: datatrain.csv, datatest.csv')
 
     # ML
-    print()
-    print('==== Training machine learning ====')
-    print()
-
-    kernel = ['linear', 'rbf', 'poly']
+    print('==== Training SVM ====')
+    
+    kernel = ['linear', 'rbf', 'poly', 'sigmoid']
+    C = [0.1, 1, 10, 100, 1000]
+    gamma = [0.1, 0.01, 0.001, 0.0001, 0.00001]
+    degree = [1, 2, 3, 4, 5]
+    n_folds = 5
 
     param_grid = {
         'kernel': kernel,
-        'C': [0.1, 0.5, 1, 10, 30, 40, 50, 70, 100, 500, 1000],
-        'gamma': [0.0001, 0.001, 0.005, 0.01, 0.05, 0.07, 0.1, 0.5, 1, 5, 10, 50]
+        'C': C,
+        'gamma': gamma,
+        'degree': degree
     }
     
-    grid = GridSearchCV(estimator=SVC(), param_grid=param_grid, cv=5, refit=True)
-    grid.fit(X_train, y_train)
+    grid = GridSearchCV(estimator=SVC(), 
+        param_grid=param_grid, 
+        cv=KFold(n_splits=n_folds), 
+        refit=True)
+    grid.fit(X_train2_norm, y_train)
 
-    print('Best score: {}'.format(grid.score(X_test, y_test)))
+    print('Best score: {}'.format(grid.score(X_test2_norm, y_test)))
     print('Best params: {}'.format(grid.best_params_))
 
     # Result
-    print()
     print('==== Result ====')
-    print()
 
-    y_pred = grid.predict(X_test)
-    print('SVM Accuracy with best params: {}%'.format(accuracy_score(y_pred, y_test) * 100))
+    y_pred = grid.predict(X_test2_norm)
+    accuracy = accuracy_score(y_pred, y_test)
+
+    print('SVM Accuracy with best params: {}'.format(accuracy))
     print('Actual test label:    {}'.format(y_test))
     print('Predicted test label: {}'.format(y_pred))
 
-    # Outputing result
-    print()
-    print('==== Writing result to CSV ====')
-    print()
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1_score = f1_score(y_test, y_pred)
 
+    print('Precision: {}'.format(precision))
+    print('Recall: {}'.format(recall))
+    print('F Score: {}'.format(f1_score))
+
+    # Outputing result
+    print('==== Writing result to CSV ====')
+
+    df_test['structure'] = df_test['a'] + df_test['b'] + df_test['x']
+    structures = df_test['structure'].to_list()
     result = {
-        'structures': structures,
+        'structure': structures,
         'class': y_pred
     }
 
     df_result = pd.DataFrame(result)
+    df_result = df_result[['structure', 'class']] 
     df_result.to_csv('output.csv', index=False)
 
     print('Done writing result to output.csv')
